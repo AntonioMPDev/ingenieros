b8a6078e31ead89e733f1297f49b7be2f5a72f1f
unknown <admin@antoniomartinezperez.com>
2024-12-10 21:58:37 +0100

    up

--- wp-content/uploads/custom-css-js/404.js
+++ wp-content/uploads/custom-css-js/404.js
@@ -15,7 +15,7 @@ pages.forEach((page) => {
 		logo.src = logo.src.replace(/LOGO-HUB-Ingenieros_color-negativo-fondo-negro-1-e1733598761546/g, "LOGO-HUB-Ingenieros_color-positivo-fondo-blanco-1-e1733860237474");
 		logo.setAttribute("srcset", "");
 		// Replace everywhere fondo-negro for fondo-negro-1 in the html
-		// const imgHtml = `<img width="214" height="64" src="http://localhost:8088/wp-content/uploads/2024/12/LOGO-HUB-Ingenieros_color-positivo-fondo-blanco-1-e1733860237474.png" class="custom-logo" alt="The Hub Office" decoding="async">`
+		// const imgHtml = `<img width="214" height="64" src="https://antoniompdev.github.io/ingenieros/wp-content/uploads/2024/12/LOGO-HUB-Ingenieros_color-positivo-fondo-blanco-1-e1733860237474.png" class="custom-logo" alt="The Hub Office" decoding="async">`
 		// const replaced = logoHtml.replace(/LOGO-HUB-Ingenieros_color-negativo-fondo-negro-1-e1733598761546/g, "LOGO-HUB-Ingenieros_color-positivo-fondo-blanco-1-e1733860237474")
 		// logoImageParent.innerHTML = replaced
 
